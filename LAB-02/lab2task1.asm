454c6695f63e8b30f5ac1c39c359796f3dbad3c3
CodewithTanzeel <Tanzeelofficial@outlook.com>
2025-11-02 23:41:00 +0500

    Create lab2task1.asm

---
+++ LAB-02/lab2task1.asm
@@ -0,0 +1,13 @@
+.data
+message: .asciiz "Hello,From StewieDev"   
+
+.text
+.globl main
+
+main:
+    li $v0, 4          
+    la $a0, message    
+    syscall            
+
+    li $v0, 10         
+    syscall
