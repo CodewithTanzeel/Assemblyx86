90845dbd04f8be32f677f7b5e014981f99371964
CodewithTanzeel <Tanzeelofficial@outlook.com>
2025-12-10 11:53:24 +0500

    Update lab7task2.asm

--- Lab-07/lab7task2.asm
+++ Lab-07/lab7task2.asm
@@ -9,13 +9,11 @@ main:
     li $t0, 0xAAAAAAAA      
     li $t1, 0x0F0F0F0F   
     li $t2, 0x00FF00FF      
-
-    
+  
     or  $t3, $t0, $t1        
     nor $t4, $t0, $zero      
     and $t5, $t4, $t2        
     or  $t6, $t3, $t5       
-
 
     li $v0, 4
     la $a0, msg_in
